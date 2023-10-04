d0322f43745da6ab9ff9ae1ad7931545c83f3093
emmanuel-mwendwa <mwendwae054@gmail.com>
2023-10-04 17:44:42 +0300

    Production runs

--- server/app/models.py
+++ server/app/models.py
@@ -4,13 +4,38 @@ class Product(db.Model):
     __tablename__ = "products"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(56), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    product_id = db.Column(db.String(13), unique=True, nullable=False)
+    product_name = db.Column(db.String(56), nullable=False)
+    product_price = db.Column(db.Float, nullable=False)
+    product_description = db.Column(db.Text)
+
+    production_runs = db.relationship('ProductionRuns', backref='product', lazy='dynamic')
     
     def to_json(self):
         json_product = {
-            "name": self.name,
-            "price": self.price
+            "Product Name": self.product_name,
+            "Product Price": self.product_price,
+            "Product Description": self.product_description,
         }
 
         return json_product
+    
+
+class ProductionRuns(db.Model):
+    __tablename__ = "production_runs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flour_kneaded = db.Column(db.Integer, nullable=False)
+    oil_used = db.Column(db.Float, nullable=False)
+    packets_produced = db.Column(db.Float, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+    def to_json(self):
+        json_productionRun = {
+            "Product Name": self.product.product_name,
+            "Flour Kneaded": self.flour_kneaded,
+            "Oil Used": self.oil_used,
+            "Packets Produced": self.packets_produced
+        }
+
+        return json_productionRun
--- server/app/production/views.py
+++ server/app/production/views.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from . import production
 from .. import db
-from ..models import Product
+from ..models import Product, ProductionRuns
 
 @production.route("/")
 def production_home():
@@ -13,8 +13,10 @@ class ProductsRoutes:
         data = request.get_json()
         if request.method == "POST":
             new_product = Product (
-            name = data.get("name"),
-            price = data.get("price")
+            product_id = data.get("product_id"),
+            product_name = data.get("product_name"),
+            product_price = data.get("product_price"),
+            product_description = data.get("product_description")
             )
             db.session.add(new_product)
             db.session.commit()
@@ -43,3 +45,48 @@ class ProductsRoutes:
         db.session.delete(product)
         db.session.commit()
         return jsonify({"message": "product deleted successfully"})
+    
+
+class ProductionRunsRoutes:
+
+    @production.post('/productionruns')
+    def new_run():
+        data = request.get_json()
+        if request.method == "POST":
+            product = Product.query.get(data.get("product_id"))
+            if not product:
+                return jsonify({"message": "Product not found!"})
+            else:
+                new_run = ProductionRuns(
+                    product_id = data.get("product_id"),
+                    flour_kneaded = data.get("flour_kneaded"),
+                    oil_used = data.get("oil_used"),
+                    packets_produced = data.get("packets_produced")
+                )
+                db.session.add(new_run)
+                db.session.commit()
+        return jsonify({"message": "Run added successfully"})
+    
+    @production.get('/productionruns')
+    def view_runs():
+        runs = ProductionRuns.query.all()
+        runs_list = []
+        for run in runs:
+            runs_list.append(run.to_json())
+        return jsonify(runs_list)
+    
+    @production.get('/productionruns/<int:id>')
+    def view_run(id):
+        run = ProductionRuns.query.get(id)
+        if not run:
+            return jsonify({"message": f"Run not found!"})
+        return jsonify(run.to_json())
+    
+    @production.delete('/productionruns/<int:id>')
+    def delete_run(id):
+        run = ProductionRuns.query.get(id)
+        if not run:
+            return jsonify({"message": f"Run not found"})
+        db.session.delete(run)
+        db.session.commit()
+        return jsonify({"message": "Run deleted successfully!"})
